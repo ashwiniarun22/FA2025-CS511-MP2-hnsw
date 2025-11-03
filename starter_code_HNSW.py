b16af4a6e2011ebc3e126f27713d683f9bea2e50
Ash <ashwiniarun22@gmail.com>
2025-11-02 18:53:45 -0600

    Implemented HNSW indexing and query; added output.txt

--- starter_code_HNSW.py
+++ starter_code_HNSW.py
@@ -5,12 +5,33 @@ import os
 import requests
 
 def evaluate_hnsw():
+    dataset_path = "sift-128-euclidean.hdf5"
 
-    # start your code here
-    # download data, build index, run query
+    with h5py.File(dataset_path, "r") as f:
+        xb = f["train"][:]   
+        xq = f["test"][:]   
+    print("Train vectors:", xb.shape, " Test vectors:", xq.shape)
 
-    # write the indices of the 10 approximate nearest neighbours in output.txt, separated by new line in the same directory
+    d = xb.shape[1]          
+    M = 16
+    ef_construction = 200
+    ef_search = 200
+
+    index = faiss.IndexHNSWFlat(d, M)
+    index.hnsw.efConstruction = ef_construction
+    index.hnsw.efSearch = ef_search
+
+    print("Building index ...")
+    index.add(xb)
+    print("Index built with {} vectors".format(index.ntotal))
+
+    query_vec = xq[0:1]     
+    D, I = index.search(query_vec, 10)  
+
+    np.savetxt("output.txt", I[0], fmt="%d")
+    print("Top-10 nearest neighbor indices written to output.txt")
     
 
+
 if __name__ == "__main__":
-    evaluate_hnsw()
+        evaluate_hnsw()
